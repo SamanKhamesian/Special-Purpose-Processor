52f6e66a7b12cd77a09e987cb56b463a68f3e8d7
Saman Khamesian <saman.khamesian@gmail.com>
2019-03-24 00:53:09 +0430

    Update ALU.v

--- Source/ALU.v
+++ Source/ALU.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    17:24:43 01/13/2017 
-// Design Name: 
-// Module Name:    ALU 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module ALU
 (
    input  [7:0] input_1,
